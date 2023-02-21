ce0569c0b6d47c79643ea580658505ff9f132321
16000psi <davidjhnsmith@gmail.com>
2023-02-21 13:50:05 +0000

    complete 6 get-api-reviews-reviewid-comments

---
+++ models/fetch-comments-by-review.js
@@ -0,0 +1,22 @@
+const db = require("./../db/connection")
+const fetchReviewById = require("./fetch-review-byId")
+
+function fetchCommentsByReview (review_id) {
+
+    return fetchReviewById(review_id).then((result) => {
+        if (result.length === 0) {
+            return Promise.reject("Item not found.");
+        }
+    }).then(() => {
+    return db.query(`SELECT * FROM comments
+    where review_id = $1;`, [review_id])
+    .then(({rows}) => {
+        return rows
+    })
+
+})
+
+}
+
+
+module.exports = fetchCommentsByReview
